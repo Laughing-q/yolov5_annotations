f043c72bc0ec9c0d6cfbadbd565eec35f72300ae
shtfAdmin/qiujing <1185102784@qq.com>
2021-07-14 18:51:43 +0800

    update

--- train.py
+++ train.py
@@ -479,6 +479,43 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
 
 
 def parse_opt(known=False):
+    """
+    opt参数解析：
+    weights:加载的权重文件
+    cfg:模型配置文件，网络结构
+    data:数据集配置文件，数据集路径，类名等
+    hyp:超参数文件
+    epochs:训练总轮次
+    batch-size:批次大小
+    img-size:输入图片分辨率大小
+    rect:是否采用矩形训练，默认False
+    resume:接着打断训练上次的结果接着训练
+    nosave:不保存模型，默认False
+    notest:不进行test，默认False
+    noautoanchor:不自动调整anchor，默认False
+    evolve:是否进行超参数进化，默认False
+    bucket:谷歌云盘bucket，一般不会用到
+    cache-images:是否提前缓存图片到内存，以加快训练速度，默认False
+    image-weights:训练时对图片进行采样的权重
+    device:训练的设备，cpu；0(表示一个gpu设备cuda:0)；0,1,2,3(多个gpu设备)
+    multi-scale:是否进行多尺度训练，默认False
+    single-cls:数据集是否只有一个类别，默认False
+    adam:是否使用adam优化器
+    sync-bn:是否使用跨卡同步BN,在DDP模式使用
+    workers:dataloader的最大worker数量
+    projects:训练日志，权重保存的目录
+    entity:
+    name:数据集名字，如果设置：results.txt to results_name.txt，默认无
+    exist_ok:
+    quad:
+    linear-lr:
+    label-smoothing:
+    upload_dataset:
+    bbox_interval:
+    save_period:
+    aftifact_alias:
+    local_rank:gpu编号
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', type=str, default='yolov5s.pt', help='initial weights path')
     parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
